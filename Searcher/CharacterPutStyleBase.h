02b6335df39c613d462cff6aff2231ea002150b9
hamity <hazemi94basic@gmail.com>
2024-05-19 04:27:28 +0900

    デストラクタを仮想関数に変更

--- Searcher/CharacterPutStyleBase.h
+++ Searcher/CharacterPutStyleBase.h
@@ -4,8 +4,14 @@ class CharacterPutStyleBase
 protected:
 	String character;
 	int purposeIndex;
+	virtual ~CharacterPutStyleBase(){};
 public:
+	enum ePutStyle
+	{
+		SquaresPut,
+	};
+
 	CharacterPutStyleBase(String charcter, int _purposeIndex);
-	virtual std::vector<Vec2> DecidePosition() = 0;
+	virtual std::vector<Vec2> CreatePosition() = 0;
 };
 
